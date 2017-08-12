1e37196d9b5b385cb47900116c26988d7ea7ca45
Rfpnj <30703739+Rfpnj@users.noreply.github.com>
2017-08-12 02:34:26 -0400

    Update ItemEffectType.java

--- src/com/base/game/inventory/item/ItemEffectType.java
+++ src/com/base/game/inventory/item/ItemEffectType.java
@@ -659,6 +659,31 @@ public enum ItemEffectType {
 		}
 	},
 	
+	FIT_SQUIRREL_JAVA(Util.newArrayListOfValues(
+			new ListValue<>("[style.boldGood(Restores)] 5% [style.boldStamina(stamina)]"),
+			new ListValue<>("[style.boldGood(+1)] [style.boldFitness(fitness)] to 'potion effects'")),
+			Colour.ATTRIBUTE_FITNESS) {
+
+		@Override
+		public List<TFModifier> getPrimaryModifiers() {
+			return null;
+		}
+
+		@Override
+		public List<TFModifier> getSecondaryModifiers(TFModifier primaryModifier) {
+			return null;
+		}
+		
+		@Override
+		public String applyEffect(TFModifier primaryModifier, TFModifier secondaryModifier, GameCharacter user, GameCharacter target) {
+			target.incrementStamina(target.getAttributeValue(Attribute.STAMINA_MAXIMUM)/20);
+
+			return "A soothing wave of arcane energy washes over you..."
+					+ "</br>"
+					+ target.addPotionEffect(Attribute.FITNESS, 1);
+		}
+	},
+	
 	SEX_HARPY_PERFUME(Util.newArrayListOfValues(
 			new ListValue<>("[style.boldSex(+1)] [style.boldFeminine(femininity)]"),
 			new ListValue<>("[style.boldGood(+1)] [style.boldFitness(fitness)] to 'potion effects'")),
@@ -856,6 +881,31 @@ public enum ItemEffectType {
 		}
 	},
 	
+	RACE_ROUND_NUTS(Util.newArrayListOfValues(
+			new ListValue<>("[style.boldGood(+3)] [style.boldFitness(fitness)] to 'potion effects'"),
+			new ListValue<>("[style.boldGood(+2)] [style.boldIntelligence(intelligence)] to 'potion effects'")),
+			Colour.RACE_SQUIRREL_MORPH) {
+		
+		@Override
+		public List<TFModifier> getPrimaryModifiers() {
+			return null;
+		}
+
+		@Override
+		public List<TFModifier> getSecondaryModifiers(TFModifier primaryModifier) {
+			return null;
+		}
+		
+		@Override
+		public String applyEffect(TFModifier primaryModifier, TFModifier secondaryModifier, GameCharacter user, GameCharacter target) {
+			return "You start to feel a lot more fit..."
+					+ "</br>"
+					+ target.addPotionEffect(Attribute.FITNESS, 3)
+					+ "</br>"
+					+ target.addPotionEffect(Attribute.INTELLIGENCE, 2);
+		}
+	},
+	
 	RACE_SUGAR_CARROT_CUBE(Util.newArrayListOfValues(
 			new ListValue<>("[style.boldGood(+3)] [style.boldStrength(strength)] to 'potion effects'"),
 			new ListValue<>("[style.boldGood(+2)] [style.boldFitness(fitness)] to 'potion effects'")),
@@ -993,6 +1043,27 @@ public enum ItemEffectType {
 		public String applyEffect(TFModifier primaryModifier, TFModifier secondaryModifier, GameCharacter user, GameCharacter target) {
 			target.incrementEssenceCount(TFEssence.CAT_MORPH, 1);
 			return "You have absorbed [style.boldGood(+1)] [style.boldCat(Cat-morph)] essence!";
+		}
+	},
+	
+	BOTTLED_ESSENCE_SQUIRREL_MORPH(Util.newArrayListOfValues(
+			new ListValue<>("[style.boldGood(+1)] [style.boldCat(Squirrel-morph)] essence")),
+			Colour.RACE_SQUIRREL_MORPH) {
+		
+		@Override
+		public List<TFModifier> getPrimaryModifiers() {
+			return null;
+		}
+
+		@Override
+		public List<TFModifier> getSecondaryModifiers(TFModifier primaryModifier) {
+			return null;
+		}
+		
+		@Override
+		public String applyEffect(TFModifier primaryModifier, TFModifier secondaryModifier, GameCharacter user, GameCharacter target) {
+			target.incrementEssenceCount(TFEssence.SQUIRREL_MORPH, 1);
+			return "You have absorbed [style.boldGood(+1)] [style.boldCat(Squirrel-morph)] essence!";
 		}
 	},
 	
@@ -1592,6 +1663,30 @@ public enum ItemEffectType {
 		@Override
 		public String applyEffect(TFModifier primaryModifier, TFModifier secondaryModifier, GameCharacter user, GameCharacter target) {
 			return getRacialEffect(Race.CAT_MORPH, primaryModifier, secondaryModifier, user, target);
+		}
+	},
+	
+	RACE_SQUIRREL_MORPH(null,
+			Colour.RACE_SQUIRREL_MORPH) {
+
+		@Override
+		public List<TFModifier> getPrimaryModifiers() {
+			return TFModifier.getTFRacialBodyPartsListList();
+		}
+
+		@Override
+		public List<TFModifier> getSecondaryModifiers(TFModifier primaryModifier) {
+			return getRacialSecondaryModifiers(primaryModifier);
+		}
+		
+		@Override
+		public List<String> getEffectsDescription(TFModifier primaryModifier, TFModifier secondaryModifier) {
+			return getRacialEffectDescription(Race.SQUIRREL_MORPH, primaryModifier, secondaryModifier);
+		}
+		
+		@Override
+		public String applyEffect(TFModifier primaryModifier, TFModifier secondaryModifier, GameCharacter user, GameCharacter target) {
+			return getRacialEffect(Race.SQUIRREL_MORPH, primaryModifier, secondaryModifier, user, target);
 		}
 	},
 	
