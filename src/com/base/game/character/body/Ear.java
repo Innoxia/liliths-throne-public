98bac996c8d24e15fdfb073e018e6b2618bb6f86
Innoxia <innoxia7@gmail.com>
2017-09-09 21:54:58 +0100

    Update Ear.java

--- src/com/base/game/character/body/Ear.java
+++ src/com/base/game/character/body/Ear.java
@@ -164,14 +164,14 @@ public class Ear implements BodyPartInterface, Serializable {
 			case COW_MORPH:
 				if (owner.isPlayer()) {
 					UtilText.transformationContentSB.append(
-								" They quickly take on a distinctly bovine shape, by growing out and narrowing down into long, slightly-folded ovals."
+								" They quickly take on a distinctly bovine shape by growing out and narrowing down into long, slightly-folded ovals."
 								+ " A layer of [pc.earFullDescriptionColour] grows to cover them, and as the transformation finishes, you discover that you're able to twitch your new cow-like ears back and forth."
 								+ "</br>"
 								+ "You now have [style.boldCowMorph(anthropomorphic, cow-like ears)]."
 							+ "</p>");
 				} else {
 					UtilText.transformationContentSB.append(
-								" They quickly take on a distinctly bovine shape, by growing out and narrowing down into long, slightly-folded ovals."
+								" They quickly take on a distinctly bovine shape by growing out and narrowing down into long, slightly-folded ovals."
 								+ " A layer of [npc.earFullDescriptionColour] grows to cover them, and as the transformation finishes, [npc.she] experimentally twitches [npc.her] new cow-like ears back and forth."
 								+ "</br>"
 								+ "[npc.Name] now has [style.boldCowMorph(anthropomorphic, cow-like ears)]."
