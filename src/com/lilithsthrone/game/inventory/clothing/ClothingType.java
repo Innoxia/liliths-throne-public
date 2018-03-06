1b6a5cb3027eecea36ed7e65890ca648a686e100
Master of Puppets <36892156+A-Master-of-Puppets@users.noreply.github.com>
2018-03-06 16:23:46 +0000

    GROIN_BOXERS
    Change displacement type of boxers to unbutton and pull down. Also adds concealedSlots to these displacements.

--- src/com/lilithsthrone/game/inventory/clothing/ClothingType.java
+++ src/com/lilithsthrone/game/inventory/clothing/ClothingType.java
@@ -7039,14 +7039,47 @@ public class ClothingType {
 			null,
 
 			Util.newArrayListOfValues(
-					new ListValue<BlockedParts>(new BlockedParts(DisplacementType.REMOVE_OR_EQUIP,
+					new ListValue<BlockedParts>(new BlockedParts(
+							DisplacementType.REMOVE_OR_EQUIP,
 							Util.newArrayListOfValues(
 									new ListValue<ClothingAccess>(ClothingAccess.LEGS_UP_TO_GROIN_LOW_LEVEL),
 									new ListValue<ClothingAccess>(ClothingAccess.LEGS_UP_TO_GROIN),
-									new ListValue<ClothingAccess>(ClothingAccess.GROIN)),
-							null, null, null)),
-					new ListValue<BlockedParts>(new BlockedParts(DisplacementType.SHIFTS_ASIDE, Util.newArrayListOfValues(new ListValue<ClothingAccess>(ClothingAccess.GROIN)),
-							Util.newArrayListOfValues(new ListValue<CoverableArea>(CoverableArea.ANUS), new ListValue<CoverableArea>(CoverableArea.PENIS), new ListValue<CoverableArea>(CoverableArea.VAGINA)), null, null))),
+									new ListValue<ClothingAccess>(ClothingAccess.GROIN)
+									),
+							null,
+							null,
+							null)),
+					new ListValue<BlockedParts>(new BlockedParts(
+							DisplacementType.UNBUTTONS,
+							Util.newArrayListOfValues(
+									new ListValue<ClothingAccess>(ClothingAccess.GROIN)
+									),
+							Util.newArrayListOfValues(
+									new ListValue<CoverableArea>(CoverableArea.PENIS)
+									),
+							null,
+							Util.newArrayListOfValues(
+									new ListValue<InventorySlot>(InventorySlot.PENIS),
+									new ListValue<InventorySlot>(InventorySlot.PIERCING_PENIS)
+									))),
+					new ListValue<BlockedParts>(new BlockedParts(
+							DisplacementType.PULLS_DOWN,
+							Util.newArrayListOfValues(
+									new ListValue<ClothingAccess>(ClothingAccess.GROIN)
+									),
+							Util.newArrayListOfValues(
+									new ListValue<CoverableArea>(CoverableArea.ANUS),
+									new ListValue<CoverableArea>(CoverableArea.PENIS),
+									new ListValue<CoverableArea>(CoverableArea.VAGINA)
+									),
+							null,
+							Util.newArrayListOfValues(
+									new ListValue<InventorySlot>(InventorySlot.PENIS),
+									new ListValue<InventorySlot>(InventorySlot.VAGINA),
+									new ListValue<InventorySlot>(InventorySlot.ANUS),
+									new ListValue<InventorySlot>(InventorySlot.PIERCING_VAGINA),
+									new ListValue<InventorySlot>(InventorySlot.PIERCING_PENIS)
+									)))),
 
 			null,
 			Colour.allClothingColours,
@@ -7078,28 +7111,6 @@ public class ClothingType {
 				else
 					return UtilText.parse(clothingOwner, "You pull [npc.name]'s boxers down and slide them off [npc.her] feet.");
 			}
-		}
-
-		@Override
-		public String displaceText(GameCharacter clothingOwner, GameCharacter clothingRemover, DisplacementType dt, boolean rough) {
-			if (clothingOwner.isPlayer() && clothingRemover.isPlayer())
-				return "You shift your boxers to one side.";
-			else if (!clothingOwner.isPlayer() && !clothingRemover.isPlayer())
-				return UtilText.parse(clothingOwner, "[npc.Name] shifts [npc.her] boxers to one side.");
-			else {
-				if (clothingOwner.isPlayer())
-					return "[npc.Name] shifts your boxers to one side.";
-				else
-					return UtilText.parse(clothingOwner, "You shift [npc.name]'s boxers to one side.");
-			}
-		}
-
-		@Override
-		public String replaceText(GameCharacter clothingOwner, GameCharacter clothingRemover, DisplacementType dt, boolean rough) {
-			if (clothingOwner.isPlayer() && clothingRemover.isPlayer())
-				return "You move your boxers back into their proper place.";
-			else
-				return UtilText.parse(clothingOwner, "[npc.Name] moves [npc.her] boxers back into their proper place.");
 		}
 	};
 
