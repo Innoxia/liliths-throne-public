e8fa8168c98caffb953949bd06d8e26aea776571
Master of Puppets <36892156+A-Master-of-Puppets@users.noreply.github.com>
2018-03-19 00:03:34 +0000

    If glow is disabled, don't bother drawing buttons

--- src/com/lilithsthrone/game/dialogue/utils/CharacterModificationUtils.java
+++ src/com/lilithsthrone/game/dialogue/utils/CharacterModificationUtils.java
@@ -3526,10 +3526,6 @@ public class CharacterModificationUtils {
 			}
 			contentSB.append("</br>");
 			if(activeCovering.getPrimaryColour() == Colour.COVERING_NONE || !withGlow) { // Disable glow:
-					contentSB.append(
-							"<div class='normal-button disabled' style='width:50%; margin:2% auto; padding:0; text-align:center;'>"
-								+ "[style.colourDisabled(Glow)]"
-							+ "</div>");
 				
 			} else {
 				if(activeCovering.isPrimaryGlowing()) {
@@ -3578,10 +3574,6 @@ public class CharacterModificationUtils {
 			}
 			contentSB.append("</br>");
 			if(activeCovering.getSecondaryColour() == Colour.COVERING_NONE || !withGlow || activeCovering.getPattern()==CoveringPattern.NONE || !withSecondary) { // Disable glow:
-					contentSB.append(
-							"<div class='normal-button disabled' style='width:50%; margin:2% auto; padding:0; text-align:center;'>"
-								+ "[style.colourDisabled(Glow)]"
-							+ "</div>");
 				
 			} else {
 				if(activeCovering.isSecondaryGlowing()) {
