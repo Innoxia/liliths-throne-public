382c2a37be8adb77696f01d924ad2ad3bde10827
Master of Puppets <36892156+A-Master-of-Puppets@users.noreply.github.com>
2018-03-28 18:31:26 +0100

    Add one-shot actions to lubricate tails

--- src/com/lilithsthrone/game/sex/sexActions/baseActionsSelfPlayer/PlayerSelfTailMouth.java
+++ src/com/lilithsthrone/game/sex/sexActions/baseActionsSelfPlayer/PlayerSelfTailMouth.java
@@ -2,11 +2,14 @@ package com.lilithsthrone.game.sex.sexActions.baseActionsSelfPlayer;
 
 import com.lilithsthrone.game.character.attributes.CorruptionLevel;
 import com.lilithsthrone.game.sex.ArousalIncrease;
+import com.lilithsthrone.game.sex.LubricationType;
 import com.lilithsthrone.game.sex.OrificeType;
 import com.lilithsthrone.game.sex.PenetrationType;
+import com.lilithsthrone.game.sex.Sex;
 import com.lilithsthrone.game.sex.SexParticipantType;
 import com.lilithsthrone.game.sex.sexActions.SexAction;
 import com.lilithsthrone.game.sex.sexActions.SexActionType;
+import com.lilithsthrone.main.Main;
 
 /**
  * @since 0.1.79
@@ -14,6 +17,42 @@ import com.lilithsthrone.game.sex.sexActions.SexActionType;
  * @author Innoxia
  */
 public class PlayerSelfTailMouth {
+
+	public static final SexAction PLAYER_SELF_TAIL_MOUTH_LUBRICATION = new SexAction(
+			SexActionType.PLAYER_REQUIRES_NO_PENETRATION_AND_EXPOSED,
+			ArousalIncrease.ONE_MINIMUM,
+			ArousalIncrease.ZERO_NONE,
+			CorruptionLevel.ZERO_PURE,
+			PenetrationType.TAIL,
+			OrificeType.MOUTH,
+			SexParticipantType.SELF) {
+		
+		@Override
+		public boolean isBaseRequirementsMet() {
+			return !Sex.getWetPenetrationTypes(Main.game.getPlayer()).get(PenetrationType.TAIL).contains(LubricationType.PLAYER_SALIVA);
+		}
+		
+		@Override
+		public String getActionTitle() {
+			return "Lubricate tail";
+		}
+
+		@Override
+		public String getActionDescription() {
+			return "Lubricate your tail with your saliva.";
+		}
+
+		@Override
+		public String getDescription() {
+			return "Lifting your [pc.hand] up to your mouth, you slide your [pc.tail+] past your [pc.lips+], before lewdly sucking on it for a moment in order to get it well lubricated with your saliva.";
+		}
+		
+		@Override
+		public void applyEffects() {
+			Sex.transferLubrication(Main.game.getPlayer(), Main.game.getPlayer(), PenetrationType.TAIL, OrificeType.MOUTH);
+		}
+	};
+	
 	public static final SexAction PLAYER_SELF_TAIL_MOUTH_PENETRATION = new SexAction(
 			SexActionType.PLAYER_PENETRATION,
 			ArousalIncrease.ONE_MINIMUM,
