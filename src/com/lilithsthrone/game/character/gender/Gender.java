dc81bdd20eb6ce28d252217c416360094dd62c92
Master of Puppets <36892156+A-Master-of-Puppets@users.noreply.github.com>
2018-04-17 22:58:10 +0100

    Add Gender.toMasculine, Gender.toFeminine, and Gender.toAndro methods

--- src/com/lilithsthrone/game/character/gender/Gender.java
+++ src/com/lilithsthrone/game/character/gender/Gender.java
@@ -1,5 +1,8 @@
 package com.lilithsthrone.game.character.gender;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.lilithsthrone.main.Main;
 import com.lilithsthrone.utils.Colour;
 
@@ -149,5 +152,92 @@ public enum Gender {
 
 	public GenderPreference getGenderPreferenceDefault() {
 		return genderPreferenceDefault;
+	}
+
+	public Gender toMasculine() {
+		Map<Gender, Gender> map = new HashMap<Gender, Gender>();
+		map.put(M_P_V_B_HERMAPHRODITE, M_P_V_B_HERMAPHRODITE);
+		map.put(M_P_V_HERMAPHRODITE, M_P_V_HERMAPHRODITE);
+		map.put(M_P_B_BUSTYBOY, M_P_B_BUSTYBOY);
+		map.put(M_P_MALE, M_P_MALE);
+		map.put(M_V_B_BUTCH, M_V_B_BUTCH);
+		map.put(M_V_CUNTBOY, M_V_CUNTBOY);
+		map.put(M_B_MANNEQUIN, M_B_MANNEQUIN);
+		map.put(M_MANNEQUIN, M_MANNEQUIN);
+		map.put(F_P_V_B_FUTANARI, M_P_V_B_HERMAPHRODITE);
+		map.put(F_P_V_FUTANARI, M_P_V_HERMAPHRODITE);
+		map.put(F_P_B_SHEMALE, M_P_B_BUSTYBOY);
+		map.put(F_P_TRAP, M_P_MALE);
+		map.put(F_V_B_FEMALE, M_V_B_BUTCH);
+		map.put(F_V_FEMALE, M_V_CUNTBOY);
+		map.put(F_B_DOLL, M_B_MANNEQUIN);
+		map.put(F_DOLL, M_MANNEQUIN);
+		map.put(N_P_V_B_HERMAPHRODITE, M_P_V_B_HERMAPHRODITE);
+		map.put(N_P_V_HERMAPHRODITE, M_P_V_HERMAPHRODITE);
+		map.put(N_P_B_SHEMALE, M_P_B_BUSTYBOY);
+		map.put(N_P_TRAP, M_P_MALE);
+		map.put(N_V_B_TOMBOY, M_V_B_BUTCH);
+		map.put(N_V_TOMBOY, M_V_CUNTBOY);
+		map.put(N_B_DOLL, M_B_MANNEQUIN);
+		map.put(N_NEUTER, M_MANNEQUIN);
+		return map.get(this);
+	}
+
+	public Gender toFeminine() {
+		Map<Gender, Gender> map = new HashMap<Gender, Gender>();
+		map.put(M_P_V_B_HERMAPHRODITE, F_P_V_B_FUTANARI);
+		map.put(M_P_V_HERMAPHRODITE, F_P_V_FUTANARI);
+		map.put(M_P_B_BUSTYBOY, F_P_B_SHEMALE);
+		map.put(M_P_MALE, F_P_TRAP);
+		map.put(M_V_B_BUTCH, F_V_B_FEMALE);
+		map.put(M_V_CUNTBOY, F_V_FEMALE);
+		map.put(M_B_MANNEQUIN, F_B_DOLL);
+		map.put(M_MANNEQUIN, F_DOLL);
+		map.put(F_P_V_B_FUTANARI, F_P_V_B_FUTANARI);
+		map.put(F_P_V_FUTANARI, F_P_V_FUTANARI);
+		map.put(F_P_B_SHEMALE, F_P_B_SHEMALE);
+		map.put(F_P_TRAP, F_P_TRAP);
+		map.put(F_V_B_FEMALE, F_V_B_FEMALE);
+		map.put(F_V_FEMALE, F_V_FEMALE);
+		map.put(F_B_DOLL, F_B_DOLL);
+		map.put(F_DOLL, F_DOLL);
+		map.put(N_P_V_B_HERMAPHRODITE, F_P_V_B_FUTANARI);
+		map.put(N_P_V_HERMAPHRODITE, F_P_V_FUTANARI);
+		map.put(N_P_B_SHEMALE, F_P_B_SHEMALE);
+		map.put(N_P_TRAP, F_P_TRAP);
+		map.put(N_V_B_TOMBOY, F_V_B_FEMALE);
+		map.put(N_V_TOMBOY, F_V_FEMALE);
+		map.put(N_B_DOLL, F_B_DOLL);
+		map.put(N_NEUTER, F_DOLL);
+		return map.get(this);
+	}
+
+	public Gender toAndrogynous() {
+		Map<Gender, Gender> map = new HashMap<Gender, Gender>();
+		map.put(M_P_V_B_HERMAPHRODITE, N_P_V_B_HERMAPHRODITE);
+		map.put(M_P_V_HERMAPHRODITE, N_P_V_HERMAPHRODITE);
+		map.put(M_P_B_BUSTYBOY, N_P_B_SHEMALE);
+		map.put(M_P_MALE, N_P_TRAP);
+		map.put(M_V_B_BUTCH, N_V_B_TOMBOY);
+		map.put(M_V_CUNTBOY, N_V_TOMBOY);
+		map.put(M_B_MANNEQUIN, N_B_DOLL);
+		map.put(M_MANNEQUIN, N_NEUTER);
+		map.put(F_P_V_B_FUTANARI, N_P_V_B_HERMAPHRODITE);
+		map.put(F_P_V_FUTANARI, N_P_V_HERMAPHRODITE);
+		map.put(F_P_B_SHEMALE, N_P_B_SHEMALE);
+		map.put(F_P_TRAP, N_P_TRAP);
+		map.put(F_V_B_FEMALE, N_V_B_TOMBOY);
+		map.put(F_V_FEMALE, N_V_TOMBOY);
+		map.put(F_B_DOLL, N_B_DOLL);
+		map.put(F_DOLL, N_NEUTER);
+		map.put(N_P_V_B_HERMAPHRODITE, N_P_V_B_HERMAPHRODITE);
+		map.put(N_P_V_HERMAPHRODITE, N_P_V_HERMAPHRODITE);
+		map.put(N_P_B_SHEMALE, N_P_B_SHEMALE);
+		map.put(N_P_TRAP, N_P_TRAP);
+		map.put(N_V_B_TOMBOY, N_V_B_TOMBOY);
+		map.put(N_V_TOMBOY, N_V_TOMBOY);
+		map.put(N_B_DOLL, N_B_DOLL);
+		map.put(N_NEUTER, N_NEUTER);
+		return map.get(this);
 	}
 }
