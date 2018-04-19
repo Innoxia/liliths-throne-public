a497e9a5a18929d39a70ff94fe75eef2e627cfd9
ltpatch <>
2018-04-18 20:36:46 -0700

    Fix PC references to NPC in Vixen's Virility effect description

--- src/com/lilithsthrone/game/inventory/item/ItemEffectType.java
+++ src/com/lilithsthrone/game/inventory/item/ItemEffectType.java
@@ -477,13 +477,13 @@ public enum ItemEffectType {
 							+ "The little pink pill easily slides down [npc.her] throat, and within moments, [npc.she] feels "
 							+ ( target.hasVagina()
 									? "a strange, warm glow spreading from [npc.her] ovaries."
-										+ " [npc.Her] mind fogs over with an overwhelming desire to feel potent sperm spurting deep into [npc.her] "+(Main.game.getPlayer().isVisiblyPregnant()?"pussy":"womb")
+										+ " [npc.Her] mind fogs over with an overwhelming desire to feel potent sperm spurting deep into [npc.her] "+(target.isVisiblyPregnant()?"pussy":"womb")
 										+", and before [npc.she] can stop it, a little whimper escapes from between [npc.her] [npc.lips]."
 										+ (target.hasPenis()
 												?" At the same time, [npc.her] manhood begins to throb with need, and [npc.she] feels "
 												:"") 
 								:"")
-							+ (Main.game.getPlayer().hasPenis() 
+							+ (target.hasPenis()
 									? "an overpowering desire to sink deep into a fertile female's cunt and fill her with [npc.cum+]."
 									: "")
 						+ "</p>");
