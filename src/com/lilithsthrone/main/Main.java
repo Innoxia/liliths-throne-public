a770eacac7102fe6402632c89e11d08e904bab35
Innoxia <innoxia7@gmail.com>
2018-08-09 06:49:52 +0100

    v0.2.10 release - error logging

--- src/com/lilithsthrone/main/Main.java
+++ src/com/lilithsthrone/main/Main.java
@@ -69,7 +69,7 @@ public class Main extends Application {
 	public static final String VERSION_NUMBER = "0.2.10",
 			VERSION_DESCRIPTION = "Alpha";
 	
-	private final static boolean DEBUG = true;
+	private final static boolean DEBUG = false;
 
 	public static final Image WINDOW_IMAGE = new Image("/com/lilithsthrone/res/images/windowIcon32.png");
 
