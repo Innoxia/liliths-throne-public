63f0fa9f343b37d7da9ab52913fcdeaa04261879
Martin D <nenoxxcraft3@hotmail.fr>
2018-08-10 15:09:01 +0200

    Fixed issue where every body part would be listed as awoo-morph if the person was a wolf-morph, even lesser

--- src/com/lilithsthrone/controller/eventListeners/TooltipInformationEventListener.java
+++ src/com/lilithsthrone/controller/eventListeners/TooltipInformationEventListener.java
@@ -937,11 +937,10 @@ public class TooltipInformationEventListener implements EventListener {
 	
 	private String getBodyPartDiv(String name, Race race, BodyCoveringType covering) {
 		String raceName;
-		if(owner.getRace() == Race.WOLF_MORPH && Main.getProperties().hasValue(PropertyValue.sillyMode)){
+		raceName = race.getName();
+
+		if(raceName.equals("wolf-morph") && Main.getProperties().hasValue(PropertyValue.sillyMode)){
 			raceName = "awoo-morph";
-		}
-		else{
-			raceName = race.getName();
 		}
 		return "<div class='subTitle' style='font-weight:normal; text-align:left; margin-top:2px; white-space: nowrap;'>"+ name + ": <span style='color:" + race.getColour().toWebHexString() + ";'>"+ Util.capitaliseSentence(raceName) + "</span> - "
 					+ owner.getCovering(covering).getColourDescriptor(owner, true, true) + " " + owner.getCovering(covering).getName(owner)+"</div>";
