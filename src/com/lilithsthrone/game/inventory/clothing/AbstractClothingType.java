753764fe8abe4ea940f7d30893005143032dfe97
picobyte <PiC.o0.o.BytE@gmail.com>
2018-08-23 00:18:26 +0200

    physical resistence has become 255 max, but if we ever we want to allow negative physical resistance, then this should make sure that works as well up to -255.
    Signed-off-by: picobyte <PiC.o0.o.BytE@gmail.com>

--- src/com/lilithsthrone/game/inventory/clothing/AbstractClothingType.java
+++ src/com/lilithsthrone/game/inventory/clothing/AbstractClothingType.java
@@ -1812,11 +1812,11 @@ public abstract class AbstractClothingType extends AbstractCoreType {
 	}
 
 	public int getPhysicalResistance() {
-		return physicalResistance & 0xff;
+		return physicalResistance & ~0xff00;
 	}
 
 	public int getResistancePercentage() {
-		return physicalResistance >> 8;
+		return (physicalResistance >> 8) & 0xff;
 	}
 
 	public void setPhysicalResistance(int physicalResistance) {
