4b7bad1ff032679b01e01cc6953b31a9749352cf
fetishgal <i.swanstrom.90@gmail.com>
2018-11-03 05:33:40 +0100

    Suggested refactoring method to reduce the number of switch statements

--- src/com/lilithsthrone/game/character/body/types/EyeType.java
+++ src/com/lilithsthrone/game/character/body/types/EyeType.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+
+import org.apache.commons.lang3.StringUtils;
 
 import com.lilithsthrone.game.character.GameCharacter;
 import com.lilithsthrone.game.character.body.Body;
@@ -17,50 +20,254 @@ import com.lilithsthrone.game.dialogue.utils.UtilText;
  * @author Innoxia
  */
 public enum EyeType implements BodyPartTypeInterface {
-	HUMAN(BodyCoveringType.EYE_HUMAN, Race.HUMAN, 1, EyeShape.ROUND, EyeShape.ROUND),
+	HUMAN(
+			BodyCoveringType.EYE_HUMAN,
+			Race.HUMAN,
+			1,
+			EyeShape.ROUND,
+			EyeShape.ROUND,
+			"",
+			(Boolean isPc) -> {
+				return (isPc ? EyeTransformationText.PC_START + EyeTransformationText.PC_TO_HUMAN
+						: EyeTransformationText.NPC_START + EyeTransformationText.NPC_TO_HUMAN) + EyeTransformationText.FINISH;
+				}
+		),
 
-	ANGEL(BodyCoveringType.EYE_ANGEL, Race.ANGEL, 1, EyeShape.ROUND, EyeShape.ROUND),
+	ANGEL(
+			BodyCoveringType.EYE_ANGEL,
+			Race.ANGEL,
+			1,
+			EyeShape.ROUND,
+			EyeShape.ROUND,
+			"angelic",
+			(Boolean isPc) -> {
+				return (isPc ? EyeTransformationText.PC_START : EyeTransformationText.NPC_START) + EyeTransformationText.FINISH;
+				}
+		),
 
-	COW_MORPH(BodyCoveringType.EYE_COW_MORPH, Race.COW_MORPH, 1, EyeShape.ROUND, EyeShape.ROUND),
+	COW_MORPH(
+			BodyCoveringType.EYE_COW_MORPH,
+			Race.COW_MORPH,
+			1,
+			EyeShape.ROUND,
+			EyeShape.ROUND,
+			"cow-like",
+			(Boolean isPc) -> {
+				return (isPc ? EyeTransformationText.PC_START + EyeTransformationText.PC_TO_COW
+						: EyeTransformationText.NPC_START + EyeTransformationText.NPC_TO_COW) + EyeTransformationText.FINISH;
+				}
+		),
 
-	DEMON_COMMON(BodyCoveringType.EYE_DEMON_COMMON, Race.DEMON, 1, EyeShape.ROUND, EyeShape.VERTICAL),
+	DEMON_COMMON(
+			BodyCoveringType.EYE_DEMON_COMMON,
+			Race.DEMON,
+			1,
+			EyeShape.ROUND,
+			EyeShape.VERTICAL,
+			"demonic",
+			(Boolean isShortStature) -> {
+				return (isShortStature ? EyeTransformationText.NPC_START + EyeTransformationText.NPC_TO_IMP
+						: EyeTransformationText.NPC_START + EyeTransformationText.NPC_TO_DEMON) + EyeTransformationText.FINISH;
+				}
+		),
 
-	DOG_MORPH(BodyCoveringType.EYE_DOG_MORPH, Race.DOG_MORPH, 1, EyeShape.ROUND, EyeShape.ROUND),
+	DOG_MORPH(
+			BodyCoveringType.EYE_DOG_MORPH,
+			Race.DOG_MORPH,
+			1,
+			EyeShape.ROUND,
+			EyeShape.ROUND,
+			"dog-like",
+			(Boolean isPc) -> {
+				return (isPc ? EyeTransformationText.PC_START + EyeTransformationText.PC_TO_DOG
+						: EyeTransformationText.NPC_START + EyeTransformationText.NPC_TO_DOG) + EyeTransformationText.FINISH;
+				}
+		),
 
-	LYCAN(BodyCoveringType.EYE_LYCAN, Race.WOLF_MORPH, 1, EyeShape.ROUND, EyeShape.ROUND),
+	LYCAN(
+			BodyCoveringType.EYE_LYCAN,
+			Race.WOLF_MORPH,
+			1,
+			EyeShape.ROUND,
+			EyeShape.ROUND,
+			"wolf-like",
+			(Boolean isPc) -> {
+				return (isPc ? EyeTransformationText.PC_START + EyeTransformationText.PC_TO_LYCAN
+						: EyeTransformationText.NPC_START + EyeTransformationText.NPC_TO_LYCAN) + EyeTransformationText.FINISH;
+				}
+		),
 	
-	FOX_MORPH(BodyCoveringType.EYE_FOX_MORPH, Race.FOX_MORPH, 1, EyeShape.ROUND, EyeShape.VERTICAL),
+	FOX_MORPH(
+			BodyCoveringType.EYE_FOX_MORPH,
+			Race.FOX_MORPH,
+			1,
+			EyeShape.ROUND,
+			EyeShape.VERTICAL,
+			"fox-like",
+			(Boolean isPc) -> {
+				return (isPc ? EyeTransformationText.PC_START + EyeTransformationText.PC_TO_FOX
+						: EyeTransformationText.NPC_START + EyeTransformationText.NPC_TO_FOX) + EyeTransformationText.FINISH;
+				}
+		),
 
-	CAT_MORPH(BodyCoveringType.EYE_FELINE, Race.CAT_MORPH, 1, EyeShape.ROUND, EyeShape.VERTICAL),
+	CAT_MORPH(
+			BodyCoveringType.EYE_FELINE,
+			Race.CAT_MORPH,
+			1,
+			EyeShape.ROUND,
+			EyeShape.VERTICAL,
+			"cat-like",
+			(Boolean isPc) -> {
+				return (isPc ? EyeTransformationText.PC_START + EyeTransformationText.PC_TO_CAT
+						: EyeTransformationText.NPC_START + EyeTransformationText.NPC_TO_CAT) + EyeTransformationText.FINISH;}
+		),
 
-	SQUIRREL_MORPH(BodyCoveringType.EYE_SQUIRREL, Race.SQUIRREL_MORPH, 1, EyeShape.ROUND, EyeShape.ROUND),
+	SQUIRREL_MORPH(
+			BodyCoveringType.EYE_SQUIRREL,
+			Race.SQUIRREL_MORPH,
+			1,
+			EyeShape.ROUND,
+			EyeShape.ROUND,
+			"squirrel-like",
+			(Boolean isPc) -> {
+				return (isPc ? EyeTransformationText.PC_START + EyeTransformationText.PC_TO_SQUIRREL
+						: EyeTransformationText.NPC_START + EyeTransformationText.NPC_TO_SQUIRREL) + EyeTransformationText.FINISH;}
+		),
 
-	RAT_MORPH(BodyCoveringType.EYE_RAT, Race.RAT_MORPH, 1, EyeShape.ROUND, EyeShape.ROUND),
+	RAT_MORPH(
+			BodyCoveringType.EYE_RAT,
+			Race.RAT_MORPH,
+			1,
+			EyeShape.ROUND,
+			EyeShape.ROUND,
+			"rat-like",
+			(Boolean isPc) -> {
+				return (isPc ? EyeTransformationText.PC_START + EyeTransformationText.PC_TO_RAT
+						: EyeTransformationText.NPC_START + EyeTransformationText.NPC_TO_RAT) + EyeTransformationText.FINISH;
+				}
+		),
 
-	RABBIT_MORPH(BodyCoveringType.EYE_RABBIT, Race.RABBIT_MORPH, 1, EyeShape.ROUND, EyeShape.ROUND),
+	RABBIT_MORPH(
+			BodyCoveringType.EYE_RABBIT,
+			Race.RABBIT_MORPH,
+			1,
+			EyeShape.ROUND,
+			EyeShape.ROUND,
+			"rabbit-like",
+			(Boolean isPc) -> {
+				return (isPc ? EyeTransformationText.PC_START + EyeTransformationText.PC_TO_RABBIT
+						: EyeTransformationText.NPC_START + EyeTransformationText.NPC_TO_RABBIT) + EyeTransformationText.FINISH;
+				}
+		),
 	
-	BAT_MORPH(BodyCoveringType.EYE_BAT, Race.BAT_MORPH, 1, EyeShape.ROUND, EyeShape.ROUND),
+	BAT_MORPH(
+			BodyCoveringType.EYE_BAT,
+			Race.BAT_MORPH,
+			1,
+			EyeShape.ROUND,
+			EyeShape.ROUND,
+			"bat-like",
+			(Boolean isPc) -> {
+				return (isPc ? EyeTransformationText.PC_START + EyeTransformationText.PC_TO_BAT
+						: EyeTransformationText.NPC_START + EyeTransformationText.NPC_TO_BAT) + EyeTransformationText.FINISH;
+				}
+		),
 
-	ALLIGATOR_MORPH(BodyCoveringType.EYE_ALLIGATOR_MORPH, Race.ALLIGATOR_MORPH, 1, EyeShape.ROUND, EyeShape.VERTICAL),
+	ALLIGATOR_MORPH(
+			BodyCoveringType.EYE_ALLIGATOR_MORPH,
+			Race.ALLIGATOR_MORPH,
+			1,
+			EyeShape.ROUND,
+			EyeShape.VERTICAL,
+			"reptile-like",
+			(Boolean isPc) -> {
+				return (isPc ? EyeTransformationText.PC_START + EyeTransformationText.PC_TO_ALLIGATOR
+						: EyeTransformationText.NPC_START + EyeTransformationText.NPC_TO_ALLIGATOR) + EyeTransformationText.FINISH;
+				}
+		),
 
-	HORSE_MORPH(BodyCoveringType.EYE_HORSE_MORPH, Race.HORSE_MORPH, 1, EyeShape.ROUND, EyeShape.HORIZONTAL),
+	HORSE_MORPH(
+			BodyCoveringType.EYE_HORSE_MORPH,
+			Race.HORSE_MORPH,
+			1,
+			EyeShape.ROUND,
+			EyeShape.HORIZONTAL,
+			"horse-like",
+			(Boolean isPc) -> {
+				return (isPc ? EyeTransformationText.PC_START + EyeTransformationText.PC_TO_HORSE
+						: EyeTransformationText.NPC_START + EyeTransformationText.NPC_TO_HORSE) + EyeTransformationText.FINISH;
+				}
+		),
 
-	REINDEER_MORPH(BodyCoveringType.EYE_REINDEER_MORPH, Race.REINDEER_MORPH, 1, EyeShape.ROUND, EyeShape.HORIZONTAL),
+	REINDEER_MORPH(
+			BodyCoveringType.EYE_REINDEER_MORPH,
+			Race.REINDEER_MORPH,
+			1,
+			EyeShape.ROUND,
+			EyeShape.HORIZONTAL,
+			"reindeer-like",
+			(Boolean isPc) -> {
+				return (isPc ? EyeTransformationText.PC_START + EyeTransformationText.PC_TO_REINDEER
+						: EyeTransformationText.NPC_START + EyeTransformationText.NPC_TO_REINDEER) + EyeTransformationText.FINISH;
+				}
+		),
 
-	HARPY(BodyCoveringType.EYE_HARPY, Race.HARPY, 1, EyeShape.ROUND, EyeShape.ROUND);
+	HARPY(
+			BodyCoveringType.EYE_HARPY,
+			Race.HARPY,
+			1,
+			EyeShape.ROUND,
+			EyeShape.ROUND,
+			"bird-like",
+			(Boolean isPc) -> {
+				return (isPc ? EyeTransformationText.PC_START + EyeTransformationText.PC_TO_HARPY
+						: EyeTransformationText.NPC_START + EyeTransformationText.NPC_TO_HARPY) + EyeTransformationText.FINISH;
+				}
+		);
 
-	
 	private BodyCoveringType coveringType;
 	private Race race;
 	private int eyePairs;
-	private EyeShape irisShape, pupilShape;
+	private EyeShape irisShape;
+	private EyeShape pupilShape;
+	private String descriptor;
+	private Function<Boolean, String> transformationText;
 
-	private EyeType(BodyCoveringType coveringType, Race race, int eyePairs, EyeShape irisShape, EyeShape pupilShape) {
+	private EyeType(
+			BodyCoveringType coveringType,
+			Race race,
+			int eyePairs,
+			EyeShape irisShape,
+			EyeShape pupilShape,
+			String descriptor, 
+			Function<Boolean, String> transformationText) {
 		this.coveringType = coveringType;
 		this.race = race;
 		this.eyePairs = eyePairs;
 		this.irisShape = irisShape;
 		this.pupilShape = pupilShape;
+		this.descriptor = descriptor;
+		this.transformationText = transformationText;
+	}
+	
+	public Race getRace() {
+		return this.race;
+	}
+	
+	public int getEyePairs() {
+		return this.eyePairs;
+	}
+	
+	public EyeShape getIrisShape() {
+		return this.irisShape;
+	}
+	
+	public EyeShape getPupilShape() {
+		return this.pupilShape;
+	}
+	
+	public Function<Boolean, String> getTransformationText() {
+		return this.transformationText;
 	}
 
 	/**
@@ -95,41 +302,7 @@ public enum EyeType implements BodyPartTypeInterface {
 
 	@Override
 	public String getDescriptor(GameCharacter gc) {
-		switch(this){
-			case ANGEL:
-				return UtilText.returnStringAtRandom("angelic");
-			case CAT_MORPH:
-				return UtilText.returnStringAtRandom("cat-like");
-			case COW_MORPH:
-				return UtilText.returnStringAtRandom("cow-like");
-			case DEMON_COMMON:
-				return UtilText.returnStringAtRandom("demonic");
-			case DOG_MORPH:
-				return UtilText.returnStringAtRandom("dog-like");
-			case SQUIRREL_MORPH:
-				return UtilText.returnStringAtRandom("squirrel-like");
-			case ALLIGATOR_MORPH:
-				return UtilText.returnStringAtRandom("reptile-like");
-			case HARPY:
-				return UtilText.returnStringAtRandom("bird-like");
-			case HORSE_MORPH:
-				return UtilText.returnStringAtRandom("horse-like");
-			case REINDEER_MORPH:
-				return UtilText.returnStringAtRandom("reindeer-like");
-			case HUMAN:
-				return UtilText.returnStringAtRandom("");
-			case LYCAN:
-				return UtilText.returnStringAtRandom("wolf-like");
-			case FOX_MORPH:
-				return UtilText.returnStringAtRandom("fox-like");
-			case RAT_MORPH:
-				return UtilText.returnStringAtRandom("rat-like");
-			case RABBIT_MORPH:
-				return UtilText.returnStringAtRandom("rabbit-like");
-			case BAT_MORPH:
-				return UtilText.returnStringAtRandom("bat-like");
-		}
-		return "";
+		return UtilText.returnStringAtRandom(StringUtils.split(this.descriptor, ','));
 	}
 
 	@Override
@@ -137,23 +310,6 @@ public enum EyeType implements BodyPartTypeInterface {
 		return coveringType;
 	}
 
-	@Override
-	public Race getRace() {
-		return race;
-	}
-
-	public int getEyePairs() {
-		return eyePairs;
-	}
-
-	public EyeShape getIrisShape() {
-		return irisShape;
-	}
-
-	public EyeShape getPupilShape() {
-		return pupilShape;
-	}
-	
 	private static Map<Race, List<EyeType>> typesMap = new HashMap<>();
 	public static List<EyeType> getEyeTypes(Race r) {
 		if(typesMap.containsKey(r)) {
@@ -169,4 +325,137 @@ public enum EyeType implements BodyPartTypeInterface {
 		typesMap.put(r, types);
 		return types;
 	}
+	
+	
+}
+
+final class EyeTransformationText {
+	
+	static final String PC_START = "<p>"
+			+ "Your [pc.eyes] suddenly grow hot and itchy, and you instinctively scrunch them up tight as you reach up to rub at them.";
+	
+	static final String NPC_START = "<p>"
+			+ "[npc.NamePos] [npc.eyes] suddenly grow hot and itchy, and [npc.she] instinctively scrunches them up tight as [npc.she] reaches up to rub at them.";
+	
+	static final String PC_TO_HUMAN = " By the time you hesitantly open them again, they've changed into human eyes, with normally-proportioned irises and pupils."
+			+ "<br/>"
+			+ "You now have [style.boldHuman(human eyes)]";
+	static final String NPC_TO_HUMAN = " By the time [npc.she] hesitantly opens them again, they've changed into human eyes, with normally-proportioned irises and pupils."
+			+ "<br/>"
+			+ "[npc.Name] now has [style.boldHuman(human eyes)]";
+	
+	static final String NPC_TO_DEMON = " By the time [npc.she] hesitantly [npc.verb(open)] them again, they've changed into demonic eyes, with vertical pupils and large irises."
+			+ "<br/>"
+			+ "[npc.Name] now [npc.has] [style.boldDemon(demonic eyes)]";
+	
+	static final String NPC_TO_IMP = " By the time [npc.she] hesitantly [npc.verb(open)] them again, they've changed into impish eyes, with vertical pupils and large irises."
+			+ "<br/>"
+			+ "[npc.Name] now [npc.has] [style.boldImp(impish eyes)]";
+	
+	static final String PC_TO_COW = " By the time you hesitantly open them again, they've changed into cow-like eyes, with larger-than-average pupils and irises."
+			+ "<br/>"
+			+ "You now have [style.boldCowMorph(cow-like eyes)]";
+	
+	static final String NPC_TO_COW = " By the time [npc.she] hesitantly opens them again, they've changed into cow-like eyes, with larger-than-average pupils and irises."
+			+ "<br/>"
+			+ "[npc.Name] now has [style.boldCowMorph(cow-like eyes)]";
+	
+	static final String PC_TO_DOG = " By the time you hesitantly open them again, they've changed into dog-like eyes, with larger-than-average pupils and irises."
+			+ "<br/>"
+			+ "You now have [style.boldDogMorph(dog-like eyes)]";
+	
+	static final String NPC_TO_DOG = " By the time [npc.she] hesitantly opens them again, they've changed into dog-like eyes, with larger-than-average pupils and irises." 
+			+ "<br/>"
+			+ "[npc.Name] now has [style.boldDogMorph(dog-like eyes)]";
+	
+	static final String PC_TO_FOX = " By the time you hesitantly open them again, they've changed into fox-like eyes, with larger-than-average pupils and irises."
+			+ "</br>"
+			+ "You now have [style.boldFoxMorph(fox-like eyes)]";
+	
+	static final String NPC_TO_FOX = " By the time [npc.she] hesitantly opens them again, they've changed into fox-like eyes, with larger-than-average pupils and irises."
+			+ "</br>"
+			+ "[npc.Name] now has [style.boldFoxMorph(fox-like eyes)]";
+	
+	static final String PC_TO_LYCAN = " By the time you hesitantly open them again, they've changed into wolf-like eyes, with larger-than-average pupils and irises."
+			+ "<br/>"
+			+ "You now have [style.boldWolfMorph(wolf-like eyes)]";
+	
+	static final String NPC_TO_LYCAN = " By the time [npc.she] hesitantly opens them again, they've changed into wolf-like eyes, with larger-than-average pupils and irises."
+			+ "<br/>"
+			+ "[npc.Name] now has [style.boldWolfMorph(wolf-like eyes)]";
+	
+	static final String PC_TO_CAT = " By the time you hesitantly open them again, they've changed into cat-like eyes, with smaller-than-average pupils and large irises." 
+			+ "<br/>"
+			+ "You now have [style.boldCatMorph(cat-like eyes)]";
+			
+	static final String NPC_TO_CAT = " By the time [npc.she] hesitantly opens them again, they've changed into cat-like eyes, with smaller-than-average pupils and large irises." 
+			+ "<br/>"
+			+ "[npc.Name] now has [style.boldCatMorph(cat-like eyes)]";
+	
+	static final String PC_TO_SQUIRREL = " By the time you hesitantly open them again, they've changed into squirrel-like eyes, with larger-than-average pupils and small irises."
+			+ "<br/>"
+			+ "You now have [style.boldSquirrelMorph(squirrel-like eyes)]";
+			
+	static final String NPC_TO_SQUIRREL = " By the time [npc.she] hesitantly opens them again, they've changed into squirrel-like eyes, with larger-than-average pupils and small irises."
+			+ "<br/>"
+			+ "[npc.Name] now has [style.boldSquirrelMorph(squirrel-like eyes)]";
+	
+	static final String PC_TO_RAT = " By the time you hesitantly open them again, they've changed into rat-like eyes, with larger-than-average pupils and small irises."
+			+ "<br/>"
+			+ "You now have [style.boldRatMorph(rat-like eyes)]";
+	
+	static final String NPC_TO_RAT = " By the time [npc.she] hesitantly opens them again, they've changed into rat-like eyes, with larger-than-average pupils and small irises."
+			+ "<br/>"
+			+ "[npc.Name] now has [style.boldRatMorph(rat-like eyes)]";
+	
+	static final String PC_TO_RABBIT = " By the time you hesitantly open them again, they've changed into rabbit-like eyes, with larger-than-average pupils and small irises." 
+			+ "<br/>"
+			+ "You now have [style.boldRabbitMorph(rabbit-like eyes)]";
+			
+	static final String NPC_TO_RABBIT = " By the time [npc.she] hesitantly opens them again, they've changed into rabbit-like eyes, with larger-than-average pupils and small irises."
+			+ "<br/>"
+			+ "[npc.Name] now has [style.boldRabbitMorph(rabbit-like eyes)]";
+	
+	static final String PC_TO_BAT = " By the time you hesitantly open them again, they've changed into bat-like eyes, with larger-than-average pupils and small irises."
+			+ "<br/>"
+			+ "You now have [style.boldBatMorph(bat-like eyes)]";
+	
+	static final String NPC_TO_BAT = " By the time [npc.she] hesitantly opens them again, they've changed into bat-like eyes, with larger-than-average pupils and small irises."
+			+ "<br/>"
+			+ "[npc.Name] now has [style.boldBatMorph(bat-like eyes)]";
+	
+	static final String PC_TO_ALLIGATOR = " By the time you hesitantly open them again, they've changed into alligator-like eyes, with larger-than-average pupils and small irises."
+			+ "<br/>"
+			+ "You now have [style.boldGatorMorph(alligator-like eyes)]";
+	
+	static final String NPC_TO_ALLIGATOR = " By the time [npc.she] hesitantly opens them again, they've changed into alligator-like eyes, with larger-than-average pupils and small irises."
+			+ "<br/>"
+			+ "[npc.Name] now has [style.boldGatorMorph(alligator-like eyes)]";
+	
+	static final String PC_TO_HORSE = " By the time you hesitantly open them again, they've changed into horse-like eyes, with larger-than-average pupils and irises."
+			+ "<br/>"
+			+ "You now have [style.boldHorseMorph(horse-like eyes)]";
+	
+	static final String NPC_TO_HORSE = " By the time [npc.she] hesitantly opens them again, they've changed into horse-like eyes, with larger-than-average pupils and irises."
+			+ "<br/>"
+			+ "[npc.Name] now has [style.boldHorseMorph(horse-like eyes)]";
+	
+	static final String PC_TO_REINDEER = " By the time you hesitantly open them again, they've changed into reindeer-like eyes, with larger-than-average pupils and irises."
+			+ "<br/>"
+			+ "You now have [style.boldReindeerMorph(reindeer-like eyes)]";
+	
+	static final String NPC_TO_REINDEER = " By the time [npc.she] hesitantly opens them again, they've changed into reindeer-like eyes, with larger-than-average pupils and irises."
+			+ "<br/>"
+			+ "[npc.Name] now has [style.boldReindeerMorph(reindeer-like eyes)]";
+	
+	static final String PC_TO_HARPY = " By the time you hesitantly open them again, they've changed into bird-like eyes, with larger-than-average pupils and small irises."
+			+ "<br/>"
+			+ "You now have [style.boldHarpy(harpy eyes)]";
+	
+	static final String NPC_TO_HARPY = " By the time [npc.she] hesitantly opens them again, they've changed into bird-like eyes, with larger-than-average pupils and small irises."
+			+ "<br/>"
+			+ "[npc.Name] now has [style.boldHarpy(harpy eyes)]";
+	
+	static final String FINISH = ", with [style.boldGenericTF([npc.irisShape])], [npc.irisFullDescription(true)] and [style.boldGenericTF([npc.pupilShape])], [npc.pupilFullDescription(true)]."
+			+ "</p>";
 }
