183450a70ebd9e38bea2b3dba8efe23e68c7e6da
Innoxia <innoxia7@gmail.com>
2019-05-15 17:39:58 +0100

    added patch notes

--- src/com/lilithsthrone/game/inventory/CharacterInventory.java
+++ src/com/lilithsthrone/game/inventory/CharacterInventory.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
