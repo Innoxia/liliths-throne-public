1242ae2440720d44c99ab29e0ff33c65f1b3d589
Stadler76 <stadli@gmx.de>
2020-01-26 00:25:03 +0100

    Changed the comments into javadoc-comments for the tags

--- src/com/lilithsthrone/game/character/body/tags/FaceTypeTag.java
+++ src/com/lilithsthrone/game/character/body/tags/FaceTypeTag.java
@@ -6,16 +6,16 @@ package com.lilithsthrone.game.character.body.tags;
  * @author Stadler76
  */
 public enum FaceTypeTag {
-	// Standard aka human-ish faces. This is the default and can be left out.
+	/** Standard aka human-ish faces. This is the default and can be left out. */
 	STANDARD,
-	// Muzzled faces
+	/** Muzzled faces */
 	MUZZLE,
-	// Fangs instead of normal teeth
+	/** Fangs instead of normal teeth */
 	FANGS,
-	// Beaked faces
+	/** Beaked faces */
 	BEAK,
-	// Pointy, trianguler shark teeth (Currently NYI, but planned for the (far?) future)
+	/** Pointy, trianguler shark teeth (Currently NYI, but planned for the (far?) future) */
 	SHARK_TEETH,
-	// Being bald aka hairLength <= 0 is not unnatural for that faceType
+	/** Being bald aka hairLength == 0 is not unnatural for that faceType */
 	NATURAL_BALDNESS,
 }
