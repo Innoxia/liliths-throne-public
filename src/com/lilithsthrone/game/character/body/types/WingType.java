c5b8b5b2498a280f05ae1bf37156572b80689bd6
Innoxia <innoxia7@gmail.com>
2021-01-25 01:45:02 +0000

    0.3.15 couple of bug fixes

--- src/com/lilithsthrone/game/character/body/types/WingType.java
+++ src/com/lilithsthrone/game/character/body/types/WingType.java
@@ -280,7 +280,7 @@ public class WingType {
 		}
 		if(types.isEmpty()) {
 			for(AbstractWingType type : WingType.getAllWingTypes()) {
-				if(type.getRace()==Race.NONE) {
+				if(type.getRace()==Race.NONE && type != WingType.NONE) {
 					types.add(type);
 				}
 			}
