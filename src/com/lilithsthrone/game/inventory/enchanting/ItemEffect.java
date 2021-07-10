bec617d1ed6cfaf51d0a4da175f3a8ba4a3d9e09
James Brown <bstard13@hotmail.com>
2021-07-10 15:30:05 +0100

    ItemEffect Rewrite

--- src/com/lilithsthrone/game/inventory/enchanting/ItemEffect.java
+++ src/com/lilithsthrone/game/inventory/enchanting/ItemEffect.java
@@ -2,11 +2,7 @@ package com.lilithsthrone.game.inventory.enchanting;
 
 import java.util.List;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-import com.lilithsthrone.controller.xmlParsing.XMLUtil;
-import com.lilithsthrone.game.Game;
+import com.lilithsthrone.controller.xmlParsing.Element;
 import com.lilithsthrone.game.character.GameCharacter;
 import com.lilithsthrone.game.character.body.abstractTypes.AbstractTailType;
 import com.lilithsthrone.game.character.body.types.TailType;
@@ -15,12 +11,11 @@ import com.lilithsthrone.game.character.race.AbstractSubspecies;
 import com.lilithsthrone.game.character.race.Race;
 import com.lilithsthrone.game.character.race.RacialBody;
 import com.lilithsthrone.game.character.race.Subspecies;
-import com.lilithsthrone.main.Main;
 import com.lilithsthrone.utils.XMLSaving;
 
 /**
  * @since 0.1.8
- * @version 0.3.3.10
+ * @version 0.4.1
  * @author Innoxia
  */
 public class ItemEffect implements XMLSaving {
@@ -33,7 +28,7 @@ public class ItemEffect implements XMLSaving {
 	private AbstractItemEffectType itemEffectType;
 	private TFModifier primaryModifier;
 	private TFModifier secondaryModifier;
-	private TFPotency potency;
+	private final TFPotency potency;
 	private int limit;
 	private ItemEffectTimer timer;
 	
@@ -66,14 +61,12 @@ public class ItemEffect implements XMLSaving {
 	@Override
 	public boolean equals(Object o) {
 		if(o instanceof ItemEffect){
-			if((((ItemEffect)o).getItemEffectType()==null && itemEffectType==null
-					||((ItemEffect)o).getItemEffectType()!=null && ((ItemEffect)o).getItemEffectType().equals(itemEffectType))
-				&& ((ItemEffect)o).getPrimaryModifier() == primaryModifier
-				&& ((ItemEffect)o).getSecondaryModifier() == secondaryModifier
-				&& ((ItemEffect)o).getPotency() == potency
-				&& ((ItemEffect)o).getLimit() == limit){
-					return true;
-			}
+			return (((ItemEffect) o).getItemEffectType() == null && itemEffectType == null
+					|| ((ItemEffect) o).getItemEffectType() != null && ((ItemEffect) o).getItemEffectType().equals(itemEffectType))
+					&& ((ItemEffect) o).getPrimaryModifier() == primaryModifier
+					&& ((ItemEffect) o).getSecondaryModifier() == secondaryModifier
+					&& ((ItemEffect) o).getPotency() == potency
+					&& ((ItemEffect) o).getLimit() == limit;
 		}
 		return false;
 	}
@@ -97,143 +90,42 @@ public class ItemEffect implements XMLSaving {
 		return result;
 	}
 	
-	public Element saveAsXML(Element parentElement, Document doc) {
-		Element effect = doc.createElement("effect");
-		parentElement.appendChild(effect);
-
-		XMLUtil.addAttribute(doc, effect, "type", ItemEffectType.getIdFromItemEffectType(getItemEffectType()));
-		XMLUtil.addAttribute(doc, effect, "mod1", (getPrimaryModifier()==null?"null":getPrimaryModifier().toString()));
-		XMLUtil.addAttribute(doc, effect, "mod2", (getSecondaryModifier()==null?"null":getSecondaryModifier().toString()));
-		XMLUtil.addAttribute(doc, effect, "potency", (getPotency()==null?"null":getPotency().toString()));
-		XMLUtil.addAttribute(doc, effect, "limit", String.valueOf(getLimit()));
-		XMLUtil.addAttribute(doc, effect, "timer", String.valueOf(getTimer().getSecondsPassed()));
-		
-		return effect;
+	public boolean saveAsXML(Element parentElement) {
+		Element effectElement = parentElement.addElement("effect");
+		effectElement.addAttribute("type", ItemEffectType.getIdFromItemEffectType(itemEffectType));
+		if(primaryModifier!=null) {
+			effectElement.addAttribute("mod1", primaryModifier.toString());
+		}
+		if(secondaryModifier!=null) {
+			effectElement.addAttribute("mod2", secondaryModifier.toString());
+		}
+		if(potency!=null) {
+			effectElement.addAttribute("potency", potency.toString());
+		}
+		effectElement.addAttribute("limit", String.valueOf(limit));
+		effectElement.addAttribute("timer", String.valueOf(timer.getSecondsPassed()));
+		return true;
 	}
 	
-	public static ItemEffect loadFromXML(Element parentElement, Document doc) {
-		String itemEffectType = parentElement.getAttribute("type");
-		if(itemEffectType.isEmpty()) {
-			itemEffectType = parentElement.getAttribute("itemEffectType"); // Support for effects prior to 0.3.1.5
-		}
-
-		String primaryMod = parentElement.getAttribute("mod1");
-		if(primaryMod.isEmpty()) {
-			primaryMod = parentElement.getAttribute("primaryModifier"); // Support for effects prior to 0.3.1.5
-		}
-		
-		String secondaryMod = parentElement.getAttribute("mod2");
-		if(secondaryMod.isEmpty()) {
-			secondaryMod = parentElement.getAttribute("secondaryModifier"); // Support for effects prior to 0.3.1.5
-		}
-		
-//		if(itemEffectType.equals("RACE_DEMON")) {
-//			throw new NullPointerException();
-//		}
-		
-		switch(itemEffectType) {
-			case "ATTRIBUTE_STRENGTH":
-			case "ATTRIBUTE_FITNESS":
-				itemEffectType = "ATTRIBUTE_PHYSIQUE";
-				break;
-			case "ATTRIBUTE_INTELLIGENCE":
-				itemEffectType = "ATTRIBUTE_ARCANE";
-				break;
-		}
-		switch(primaryMod) {
-			case "DAMAGE_ATTACK":
-			case "RESISTANCE_ATTACK":
-				return null;
-			case "CLOTHING_SEALING":
-				primaryMod = "CLOTHING_SPECIAL";
-				secondaryMod = "CLOTHING_SEALING";
-				break;
-			case "CLOTHING_ENSLAVEMENT":
-				primaryMod = "CLOTHING_SPECIAL";
-				secondaryMod = "CLOTHING_ENSLAVEMENT";
-				break;
-			case "TF_PENIS":
-				if(Main.isVersionOlderThan(Game.loadingVersion, "0.3.5.1")) {
-					if(secondaryMod=="TF_MOD_SIZE_SECONDARY") {
-						secondaryMod = "TF_MOD_SIZE_TERTIARY";
-					}
-				}
-				break;
-		}
-		switch(secondaryMod) {
-			case "TF_MOD_FETISH_SEEDER":
-				secondaryMod = "TF_MOD_FETISH_IMPREGNATION";
-				break;
-			case "TF_MOD_FETISH_BROODMOTHER":
-				secondaryMod = "TF_MOD_FETISH_PREGNANCY";
-				break;
-			case "CRITICAL_CHANCE":
-				secondaryMod = "CRITICAL_DAMAGE";
-				break;
-			case "CLOTHING_ANTI_SELF_TRANSFORMATION":
-				secondaryMod = "CLOTHING_SERVITUDE";
-				break;
-			case "TF_MOD_ORIFICE_DEEP":
-				secondaryMod = "TF_MOD_DEPTH";
-				break;
-			case "TF_MOD_ORIFICE_DEEP_2":
-				secondaryMod = "TF_MOD_DEPTH_2";
-				break;
-		}
-		
-		if(Main.isVersionOlderThan(Game.loadingVersion, "0.4.1") && itemEffectType=="TF_TAIL" && primaryMod=="TF_MOD_SIZE") { // Girth TF was moved from TF_MOD_SIZE to TF_MOD_SIZE_SECONDARY in v0.4
-			primaryMod = "TF_MOD_SIZE_SECONDARY";
-		}
-		
-		ItemEffect ie;
-		try { // Wrap this in a try, as the TFModifier.valueOf might fail, due to removing Broodmother/Seeder fetish modifiers in 0.2.7.5, and then critical chance in 0.3.3.5.
-			TFModifier primary = (primaryMod.equals("null") || primaryMod.isEmpty()?null:TFModifier.valueOf(primaryMod));
-			TFModifier secondary = (secondaryMod.equals("null") || secondaryMod.isEmpty()?null:TFModifier.valueOf(secondaryMod));
-			
-			if(secondary!=null && TFModifier.getWeaponMajorAttributeList().contains(secondary)) {
-				primary = TFModifier.CLOTHING_MAJOR_ATTRIBUTE;
-			}
-			
-			// If there is no ItemEffectType with the id type, return null, as otherwise the game tries to load an incorrect ItemEffectType.
-			if(!ItemEffectType.idToItemEffectTypeMap.containsKey(itemEffectType)) {
+	public static ItemEffect loadFromXML(Element parentElement) {
+		try {
+			Element effectElement = parentElement.getMandatoryFirstOf("effect");
+			String itemEffectTypeId = effectElement.getAttribute("type");
+			// Check itemEffectType exists before retrieving it
+			if(!ItemEffectType.idToItemEffectTypeMap.containsKey(itemEffectTypeId)) {
+				System.out.println("Invalid ItemEffect type");
 				return null;
 			}
-			
-			ie = new ItemEffect(
-					ItemEffectType.getItemEffectTypeFromId(itemEffectType),
-					primary,
-					secondary,
-					(parentElement.getAttribute("potency").equals("null")?null:TFPotency.valueOf(parentElement.getAttribute("potency"))),
-					Integer.valueOf(parentElement.getAttribute("limit")));
-			
-		} catch(Exception ex) {
-			System.err.println("(Minor error, can ignore.) Unable to import ItemEffect (" + primaryMod + ", " + secondaryMod + ") from" + doc.getDocumentURI());
-			System.err.println(ex);
+			return new ItemEffect(ItemEffectType.getItemEffectTypeFromId(itemEffectTypeId),
+					TFModifier.valueOf(effectElement.getAttribute("mod1")),
+					TFModifier.valueOf(effectElement.getAttribute("mod2")),
+					TFPotency.valueOf(effectElement.getAttribute("potency")),
+					Integer.parseInt(effectElement.getAttribute("limit")));
+		} catch (Exception ex) {
+			System.out.println("Failed to load ItemEffect");
+			ex.printStackTrace();
 			return null;
 		}
-		
-		try {
-			String timerString = parentElement.getAttribute("timer");
-			int time = 0;
-			if(!timerString.isEmpty()) {
-				time = Integer.valueOf(timerString);
-			}
-			
-			if(Main.isVersionOlderThan(Game.loadingVersion, "0.2.6.5")) {// I think this was to align effects to all be on the hour:
-				int timer = time/60;
-				ie.getTimer().setSecondsPassed(((timer*60) + (int)(Main.game.getMinutesPassed()%60)));
-				
-			} else if(Main.isVersionOlderThan(Game.loadingVersion, "0.3.0.6")) {
-				ie.getTimer().setSecondsPassed(time*60);
-				
-			} else {
-				ie.getTimer().setSecondsPassed(time);
-			}
-		} catch(Exception ex) {
-			ex.printStackTrace();
-		}
-		
-		return ie;
 	}
 	
 	//TODO
@@ -316,8 +208,6 @@ public class ItemEffect implements XMLSaving {
 							} else {
 				 				return AbstractItemEffectType.getRacialEffect(halfSubspecies.getRace(), getPrimaryModifier(), secondaryMod, getPotency(), user, target).applyEffect();
 							}
-						case TF_EYES:
-			 				return AbstractItemEffectType.getRacialEffect(Race.DEMON, getPrimaryModifier(), secondaryMod, getPotency(), user, target).applyEffect();
 						case TF_ANTENNA:
 						case TF_ARMS:
 						case TF_CORE:
@@ -335,6 +225,7 @@ public class ItemEffect implements XMLSaving {
 						case TF_ASS:
 						case TF_BREASTS:
 						case TF_BREASTS_CROTCH:
+						case TF_EYES:
 						case TF_HORNS:
 						case TF_PENIS:
 						case TF_VAGINA:
