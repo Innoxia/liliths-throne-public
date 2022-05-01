9510b04382de56050729d18c2248a074f2005820
ddmaker1234 <ddmaker1234@gmail.com>
2022-05-01 13:18:09 -0500

    Patch around private remove.

--- src/com/lilithsthrone/game/Game.java
+++ src/com/lilithsthrone/game/Game.java
@@ -4680,7 +4680,7 @@ public class Game implements XMLSaving {
 		}
 	}
 
-	private void removeNPC(NPC npc) {
+	public void removeNPC(NPC npc) {
 		if(npc.isPregnant()) {
 			// End with birth if father is player
 			npc.endPregnancy(npc.getPregnantLitter().getFather()!=null && npc.getPregnantLitter().getFather().isPlayer());
