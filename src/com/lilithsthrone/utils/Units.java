2de56279b6fc9abe3cdf838e88e10963689e7cd9
IjyiH <55197151+IjyiH@users.noreply.github.com>
2022-08-05 01:44:53 +0200

    Busting duplicate line <3
    *blatantly disregards PR template*
    That line was assigning a settings value, twice. Harmless to leave there but the code looks prettier without doing extra work.
    I got rid of Discord, haven't logged in for years. Glad to see the project's still alive :D
    - IJH

--- src/com/lilithsthrone/utils/Units.java
+++ src/com/lilithsthrone/utils/Units.java
@@ -63,7 +63,6 @@ public enum Units {
             Main.getProperties().setValue(PropertyValue.metricSizes, isMetric);
             Main.getProperties().setValue(PropertyValue.metricFluids, isMetric);
             Main.getProperties().setValue(PropertyValue.metricWeights, isMetric);
-            Main.getProperties().setValue(PropertyValue.metricWeights, isMetric);
             Main.getProperties().setValue(PropertyValue.twentyFourHourTime, !twelveHourCountries.contains(countryCode));
             Main.getProperties().setValue(PropertyValue.internationalDate, isMetric);
         }
