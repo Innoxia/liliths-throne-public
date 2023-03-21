b035e603ad261e5ddc2aa71a5c659b7a140d5184
SightglassCat <126230905+SightglassCat@users.noreply.github.com>
2023-03-21 19:14:58 -0400

    Fix to missing ID and path for anonymous-classed NPCs
    (cherry picked from commit 663d15e8d53d6025a880311e22b0afefdda06765)

--- src/com/lilithsthrone/game/Game.java
+++ src/com/lilithsthrone/game/Game.java
@@ -4896,7 +4896,7 @@ public class Game implements XMLSaving {
 				npc.setId(getUniqueNPCId(npc.getClass()));
 			} else {
 				int id = npcTally.incrementAndGet();
-				npc.setId(id+","+(npc.getClass().getSimpleName()));
+				npc.setId(id+","+(npc.getClass().isAnonymousClass() ? npc.getClass().getSuperclass().getSimpleName() : npc.getClass().getSimpleName()));
 			}
 		}
 		
