1f21a0db53624fd1b206feff39269c42ba9a9262
CK_Rainbow <nanashichi@proton.me>
2023-09-29 10:33:14 +0800

    fix: fix the problem that the tooltip will be cutoff if its too long

--- src/com/lilithsthrone/controller/MainController.java
+++ src/com/lilithsthrone/controller/MainController.java
@@ -3138,8 +3138,10 @@ public class MainController implements Initializable {
 	public void setTooltipSize(int width, int height) {
 		webviewTooltip.setMaxWidth(width);
 		webviewTooltip.setMaxHeight(height);
+		webviewTooltip.setPrefHeight(height);
 		tooltip.setMaxWidth(width);
 		tooltip.setMaxHeight(height);
+		tooltip.setPrefHeight(height);
 	}
 
 	public KeyboardAction getActionToBind() {
