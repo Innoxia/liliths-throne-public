e9d69379f173781962d6e5eda24a351e383c8b15
Maxis010 <bstard13@hotmail.com>
2023-10-22 22:34:39 +0100

    Bugfix: Don't pass Slime or Imp after already checking for them

--- src/com/lilithsthrone/game/character/npc/submission/SubmissionAttacker.java
+++ src/com/lilithsthrone/game/character/npc/submission/SubmissionAttacker.java
@@ -105,6 +105,8 @@ public class SubmissionAttacker extends NPC {
 				this.setBody(gender, randomSpecies, RaceStage.GREATER, true);
 				
 			} else {
+				// Already picked a race, and it's not Slime or Imp as checked above so respect that pick
+				availableRaces = Util.newHashMapOfValues(new Util.Value<>(randomSpecies, 1));
 				this.setBodyFromSubspeciesPreference(gender, availableRaces, true, true);
 			}
 			
